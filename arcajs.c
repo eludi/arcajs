ec7f8e78ead86352c1bc724cf5896d0a974f948c
eludi <gf@eludi.net>
2020-07-27 22:28:09 +0200

    version updated

--- arcajs.c
+++ arcajs.c
@@ -14,7 +14,7 @@
 #include <string.h>
 #include <stdarg.h>
 
-const char* appVersion = "v0.20200721a";
+const char* appVersion = "v0.20200727a";
 
 static void showError(const char* msg, ...) {
 	char formattedMsg[1024];
